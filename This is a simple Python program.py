c81d900f5b995061c78965950ad2ed7604ad9104
rdlxs <r.k3nsh1n@gmail.com>
2024-06-05 10:59:31 -0300

    Creacion

---
+++ This is a simple Python program.py
@@ -0,0 +1,10 @@
+# This is a simple Python program
+
+# Print "Hello, World!" to the console
+print("Hello, World!")
+
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Greet the user by name
+print(f"Nice to meet you, {name}!")
