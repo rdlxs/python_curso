b65581d498bb35cf710beb72d79d7965f876f855
rdlxs <r.k3nsh1n@gmail.com>
2024-06-05 11:18:42 -0300

    change name

---
+++ Ejemplos/Saludo al cornudo.py
@@ -0,0 +1,10 @@
+# This is a simple Python program (by chat gpt)
+
+# Print "Hello, World!" to the console
+print("Hola Cornudo!")
+
+# Ask the user for their name
+name = input("Como te llamas ?? ")
+
+# Greet the user by name
+print(f"Alta ornamenta, {name}!")
