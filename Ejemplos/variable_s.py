01974a565eea7b5c89e5f14dda2c85ad4988331c
rdlxs <r.k3nsh1n@gmail.com>
2024-06-13 11:52:40 -0300

    .

--- Ejemplos/variable_s.py
+++ Ejemplos/variable_s.py
@@ -3,10 +3,11 @@ vendor = "Cisco"
 equipo = "ASR9k"
 ios = 7.2
 
-print(ios)
-
 #usando Type
 print(type(vendor))
 print(type(equipo))
 print(type(ios))
 
+ios = "XR 7.2"
+print(ios)
+print(type(ios))
