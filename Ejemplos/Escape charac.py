a3cd769f25461bc99bfc7f47a868e6982d9dfab5
rdlxs <r.k3nsh1n@gmail.com>
2024-06-13 10:26:44 -0300

    v2

---
+++ Ejemplos/Escape charac.py
@@ -0,0 +1,10 @@
+#generar saltos con la \
+splitstring = "Prueba de como \nel \nsimbolo \nde \nbarra \ninvertida genera un scape"
+print(splitstring)
+
+#generar tabulaciones con la \ 
+stringstabeadas = "1 \t2 \t3 \t4 \t5"
+print(stringstabeadas)
+
+#generar apostrofes
+print('Prueba para del output "Esto, eso, aquello \'e \'s, que se yo...esto es u\'n bardo" ')
