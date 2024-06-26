7c5a7c3b999272ea71c0b41f77b62b50198bd40a
rdlxs <r.k3nsh1n@gmail.com>
2024-06-26 16:53:31 -0300

    .a

--- Ejemplos/Operadores.py
+++ Ejemplos/Operadores.py
@@ -4,6 +4,12 @@ c = 17
 
 print(a + b + c)  #suma
 print(a- c + b) #resta
+print(a * b) #multiplicacion
 print(b / a) #division
 print(b // c) #division con redondeo
-print(b % a) #
+print(b % a) 
+
+print()
+
+print(a + b // 2 * 10)
+print(a + (b // 2) * 10)
