0b697bbd3873ec493e05f9ffd40bd9915b9ab69a
rdlxs <r.k3nsh1n@gmail.com>
2024-11-22 10:30:32 -0300

    change to ico

--- Ejemplos/interfaz.py
+++ Ejemplos/interfaz.py
@@ -96,7 +96,7 @@ style.theme_use("clam")
 
 from tkinter import PhotoImage
 
-icono = PhotoImage(file="C:\Users\maxgt\OneDrive\Documents\GitHub\python_curso\Ejemplos\icon.png")  
+icono = PhotoImage(file="C:\Users\maxgt\OneDrive\Documents\GitHub\python_curso\Ejemplos\icon.ico")  
 root.iconphoto(False, icono)
 
 
