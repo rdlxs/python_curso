3d2f69be2a2aa4fc19b48ca4bf0624b6f15769d1
rdlxs <r.k3nsh1n@gmail.com>
2025-09-24 14:20:53 -0300

    .

--- Ejemplos/grass.py
+++ Ejemplos/grass.py
@@ -8,7 +8,7 @@ import time
 ###sin psutil
 
 PACKAGE_NAME = "grass"
-DEB_FILENAME = "Grass_5.6.2_amd64.deb"
+DEB_FILENAME = "Grass_5.7.1_amd64.deb"
 FTP_HOST = "10.254.125.162"
 FTP_DIR = "/Repositorio/img_routers/"
 FTP_USER = "root"
