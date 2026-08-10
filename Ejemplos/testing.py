b09827d1d0699689f9a56f6c63e17241fd40f49e
auS <r.k3nsh1n@gmail.com>
2026-08-10 10:43:49 -0300

    update

--- Ejemplos/testing.py
+++ Ejemplos/testing.py
@@ -5,7 +5,7 @@ import tempfile
 import time
 
 
-DEB_FILENAME = "Grass_7.3.1_amd64.deb"
+DEB_FILENAME = "grass-desktop_7.6.0_amd64.deb"
 
 FTP_HOST = "10.254.125.162"
 FTP_DIR = "/Repositorio/img_routers/"
